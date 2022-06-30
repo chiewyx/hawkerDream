ca9a4ca8482dc569dee263d60b387066cf4b4168
Chiew Yi Xiang <yixiangchiew@gmail.com>
2022-06-30 11:30:56 +0800

    allow users to download uploaded invoices

--- src/components/invoiceCard.js
+++ src/components/invoiceCard.js
@@ -109,65 +109,91 @@ import {
   ListIcon,
   Button,
   useColorModeValue,
-} from '@chakra-ui/react';
-import { CheckIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+import { useState, useEffect, useCallback } from "react";
+import { supabase } from "../supabase";
 
 export default function ProductSimple(props) {
+  const [results, setResults] = useState("");
+
+  const getResult = useCallback(async () => {
+    //setLoading(true);
+    const user = supabase.auth.user();
+    const { data, error } = await supabase.storage
+      .from("invoices")
+      .download(`${user.id}/${props.name}/${props.supplier}`);
+    if (data) {
+      const url = URL.createObjectURL(data);
+      setResults(url);
+    }
+  }, [props.name, props.supplier]);
+
+  useEffect(() => {
+    getResult();
+  }, [getResult]);
+
   return (
     <Center py={6}>
       <Box
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        overflow={'hidden'}>
+        maxW={"330px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        overflow={"hidden"}
+      >
         <Stack
-          textAlign={'center'}
+          textAlign={"center"}
           p={6}
-          color={useColorModeValue('gray.800', 'white')}
-          align={'center'}>
+          color={useColorModeValue("gray.800", "white")}
+          align={"center"}
+        >
           <Text
-            fontSize={'lg'}
+            fontSize={"lg"}
             fontWeight={500}
-            bg={useColorModeValue('green.50', 'green.900')}
+            bg={useColorModeValue("green.50", "green.900")}
             p={2}
             px={3}
-            color={'green.500'}
-            rounded={'full'}>
+            color={"green.500"}
+            rounded={"full"}
+          >
             {props.supplier}
           </Text>
-          <Stack direction={'row'} align={'center'} justify={'center'}>
-            <Text fontSize={'3xl'}>$</Text>
-            <Text fontSize={'6xl'} fontWeight={800}>
+          <Stack direction={"row"} align={"center"} justify={"center"}>
+            <Text fontSize={"3xl"}>$</Text>
+            <Text fontSize={"6xl"} fontWeight={800}>
               {props.price}
             </Text>
           </Stack>
         </Stack>
 
-        <Box bg={useColorModeValue('gray.50', 'gray.900')} px={6} py={10}>
+        <Box bg={useColorModeValue("gray.50", "gray.900")} px={6} py={10}>
           <List spacing={3}>
-          {props.items.split(",").map((order) => (
-            <ListItem>
-              <ListIcon as={CheckIcon} color="green.400" />
-              {order}
-            </ListItem>
+            {props.items.split(",").map((order) => (
+              <ListItem>
+                <ListIcon as={CheckIcon} color="green.400" />
+                {order}
+              </ListItem>
             ))}
           </List>
 
           <Button
             mt={10}
-            w={'full'}
-            bg={'green.400'}
-            color={'white'}
-            rounded={'xl'}
-            boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
+            w={"full"}
+            bg={"green.400"}
+            color={"white"}
+            rounded={"xl"}
+            boxShadow={"0 5px 20px 0px rgb(72 187 120 / 43%)"}
             _hover={{
-              bg: 'green.500',
+              bg: "green.500",
             }}
             _focus={{
-              bg: 'green.500',
-            }}>
+              bg: "green.500",
+            }}
+            as="a"
+            href={results} download
+          >
             Invoice
           </Button>
         </Box>
