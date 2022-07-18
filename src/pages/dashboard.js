58f08e6f21400f1e13c091bd4367275657321a07
Chiew Yi Xiang <yixiangchiew@gmail.com>
2022-07-18 22:57:20 +0800

    added widgets for dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,18 +2,25 @@ import Simple from "../components/profilebar";
 import { useAuth } from "../contexts/auth";
 import { supabase } from "../supabase";
 import {
+  Box,
+  Text,
   Stack,
   Button,
-  Text,
   Flex,
   VStack,
   useBreakpointValue,
+  Heading,
+  Center,
+  HStack,
+  Spacer,
+  Grid
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 //import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 //import mainlogo from "../logo.jpg";
 import UserProfileEdit from "./update";
+import { CheckCircleIcon, CloseIcon, CalendarIcon } from "@chakra-ui/icons";
 
 export default function Dashboard() {
   //const { user } = useAuth()
@@ -25,6 +32,11 @@ export default function Dashboard() {
   const [last_name, setLastName] = useState();
   //const [avatar_url, setAvatarUrl] = useState();
   const [profile_type, setProfileType] = useState();
+  const [orderList, setOrderList] = useState([]);
+  const [incompleteNum, setIncompleteNum] = useState();
+  const [completeNum, setCompleteNum] = useState();
+  const today = new Date();
+  const month = today.toLocaleString('default', { month: 'long' });
   //const [showPassword, setShowPassword] = useState(false);
 
   async function getProfile() {
@@ -56,8 +68,36 @@ export default function Dashboard() {
     }
   }
 
+  async function fetchIncomplete() {
+    // get the orders keyed in manually by the suppliers
+    const { data, count } = await supabase
+      .from("orders")
+      .select("*", { count: "exact" })
+      .eq("user_id", user.id)
+      .eq("completed", false)
+      .order("id", true);
+
+    setOrderList(data);
+    setIncompleteNum(count);
+  }
+
+  async function fetchComplete() {
+    // get the orders keyed in manually by the suppliers
+    const { data, count } = await supabase
+      .from("orders")
+      .select("*", { count: "exact" })
+      .eq("user_id", user.id)
+      .eq("completed", true)
+      .order("id", true);
+
+    setOrderList(data);
+    setCompleteNum(count);
+  }
+
   useEffect(() => {
     getProfile();
+    fetchIncomplete();
+    fetchComplete();
   }, []);
 
   async function updateProfile(e) {
@@ -92,47 +132,102 @@ export default function Dashboard() {
   return (
     <div>
       <Simple />
-      <Flex minH={"100vh"} align={"top"} justify={"center"} bg={"gray.50"}>
-        <Flex
+      <Flex align={"top"} justify={"center"} bg={"gray.50"} minH={"100vh"}>
+        <VStack
           w={"full"}
-          h={"40vh"}
-          backgroundImage={
-            "url(https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80)"
-          }
-          backgroundSize={"cover"}
-          backgroundPosition={"center center"}
         >
-          <VStack
+          <Flex
             w={"full"}
-            justify={"center"}
-            px={useBreakpointValue({ base: 4, md: 8 })}
-            bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+            h={"40vh"}
+            backgroundImage={
+              "url(https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80)"
+            }
+            backgroundSize={"cover"}
+            backgroundPosition={"center center"}
           >
-            <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
-              <Text
-                color={"white"}
-                fontWeight={700}
-                lineHeight={1.2}
-                fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
-              >
-                Welcome back, {first_name}
-              </Text>
-              <Stack direction={"row"}>
-                <Button
-                  bg={"blue.400"}
-                  rounded={"full"}
+            <VStack
+              w={"full"}
+              justify={"center"}
+              px={useBreakpointValue({ base: 4, md: 8 })}
+              bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+            >
+              <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
+                <Text
                   color={"white"}
-                  _hover={{ bg: "blue.500" }}
-                  as={Link}
-                  to="/dashboard/update"
+                  fontWeight={700}
+                  lineHeight={1.2}
+                  fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
                 >
-                  Set-up/Update your profile!
-                </Button>
+                  Welcome back, {first_name}
+                </Text>
+                <Stack direction={"row"}>
+                  <Button
+                    bg={"blue.400"}
+                    rounded={"full"}
+                    color={"white"}
+                    _hover={{ bg: "blue.500" }}
+                    as={Link}
+                    to="/dashboard/update"
+                  >
+                    Set-up/Update your profile!
+                  </Button>
+                </Stack>
               </Stack>
-            </Stack>
-          </VStack>
-        </Flex>
-        
+            </VStack>
+          </Flex>
+          <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+          <Box
+            maxW="lg"
+            borderWidth="5px"
+            borderRadius="lg"
+            overflow="hidden"
+            m={50}
+            p={10}
+            textAlign="center"
+          >
+            <CloseIcon boxSize={"50px"} color={"red.500"} />
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+              {incompleteNum} incomplete orders
+            </Heading>
+            <Text size="lg" color={"gray.500"} as={Link} to="/order">
+              {" "}
+              View incomplete orders
+            </Text>
+          </Box>
+          <Box
+            maxW="lg"
+            borderWidth="5px"
+            borderRadius="lg"
+            overflow="hidden"
+            m={50}
+            p={10}
+            textAlign="center"
+          >
+            <CheckCircleIcon boxSize={"50px"} color={"red.500"} />
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+              {completeNum} complete orders
+            </Heading>
+          </Box>
+          <Box
+            maxW="lg"
+            borderWidth="5px"
+            borderRadius="lg"
+            overflow="hidden"
+            m={50}
+            p={10}
+            textAlign="center"
+          >
+            <CalendarIcon boxSize={"50px"} color={"red.500"} />
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+              Invoices
+            </Heading>
+            <Text size="lg" color={"gray.500"} as={Link} to={"/invoice/" + month}>
+              {" "}
+              View your invoices for {month}
+            </Text>
+          </Box>
+          </Grid>
+        </VStack>
       </Flex>
     </div>
   );
